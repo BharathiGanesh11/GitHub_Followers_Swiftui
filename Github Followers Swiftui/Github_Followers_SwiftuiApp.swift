8fd72d20eac814e69db65edd2a212081c310c6b8
BharathiGanesh11 <bharathiganesh1120@gmail.com>
2024-08-31 13:12:58 +0530

    Initial Commit

---
+++ Github Followers Swiftui/Github_Followers_SwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Followers_SwiftuiApp.swift
+//  Github Followers Swiftui
+//
+//  Created by Kumar on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_Followers_SwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
